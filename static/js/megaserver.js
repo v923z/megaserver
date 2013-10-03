2283130909560899c798f3037d47ecee434b0680
Zoltán Vörös <zvoros@gmail.com>
2013-10-03 17:52:30 +0200

    cleaned up connect_callback

--- static/js/megaserver.js
+++ static/js/megaserver.js
@@ -1,15 +1,5 @@
 function connect_callback() {
-	var message = create_message('connect', $('#baudrate').val())
-	var request = $.ajax({
-		url: 'http://127.0.0.1:8080',
-		type: 'POST',
-		data: JSON.stringify(message, null, 4),
-		dataType: 'json'
-	})
-	
-	request.done(function(msg) {
-		alert(msg)
-	})
+	send_post_request('connect', $('#baudrate').val(), dummy_callback)
 }
 
 function create_message(message_type, message_content) {
@@ -17,4 +7,19 @@ function create_message(message_type, message_content) {
 	message.command = message_type
 	message.content = message_content
 	return message
+}
+
+function send_post_request(request_type, message_content, callback_function) {
+	var message = create_message(request_type, message_content)
+	var request = $.ajax({
+		url: 'http://127.0.0.1:8080',
+		type: 'POST',
+		data: JSON.stringify(message, null, 4),
+		dataType: 'json'
+	})
+	request.done(callback_function)	
+}
+
+function dummy_callback(message) {
+	alert(message)
 }
